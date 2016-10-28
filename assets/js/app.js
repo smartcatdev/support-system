f1cd16740ebfee8ff97d5ab66279e90a18ca165a
eric Green <eric@smartcat.ca>
2016-10-28 19:32:26 -0400

    Show/hide save button and status spinner

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@ jQuery( document ).ready( function( $ ) {
             $( document ).on( 'dblclick', 'tr', TicketActions.editTicket );
             $( document ).on( 'submit', '.edit_ticket_form', TicketActions.saveTicket );
             $( document ).on( 'focus', '.form_field', TicketActions.showSaveButton );
+            //$( document ).on( 'click', '.status', TicketActions.clearStatus );
 
             $.SmartcatSupport().wp_ajax( 'list_support_tickets', null, function ( response ) {
 
@@ -45,20 +46,30 @@ jQuery( document ).ready( function( $ ) {
         saveTicket: function ( e ) {
             e.preventDefault();
 
-            $( this ).find( '.status' ).html( '<div class="spinner"></div>' );
+            var form = $( this );
+
+            form.children( '.status' ).html( '<div class="spinner"></div>' );
 
             $.SmartcatSupport().wp_ajax( 'save_support_ticket', $( this ).serializeArray(), function ( response ) {
 
                 setTimeout( function () {
-                    $( '.spinner' ).delay( 20000 ).removeClass( 'spinner' ).addClass( 'icon-checkmark' );
+
+                    $( '.spinner' ).removeClass( 'spinner' ).addClass( 'icon-checkmark' );
+                    form.children( '.submit_button' ).hide();
+
                 }, 2000 );
 
             } );
+
+            setTimeout( function () {
+
+                $( '.status' ).empty();
+
+            }, 4000 );
         },
 
-        showSaveButton: function() {
-            $( this ).parents( '.edit_ticket_form' ).find( '.submit_button' ).removeClass( 'hidden' );
-            $( this ).parents( '.edit_ticket_form' ).find( '.status' ).empty();
+        showSaveButton: function () {
+            $( this ).parents( '.edit_ticket_form' ).find( '.submit_button' ).show();
         }
 
     };
