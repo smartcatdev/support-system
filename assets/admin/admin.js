400ac26e30c891452162bbbc4c47b2b0ae334510
Eric Green <eric@smartcat.ca>
2016-12-13 09:33:34 -0500

    Add check for inlineEditPost

--- assets/admin/admin.js
+++ assets/admin/admin.js
@@ -25,33 +25,37 @@ jQuery(document).ready(function ($) {
 
     $('.support_admin_toggle[name="flagged"]').on('click', toggleFlag);
 
-    // var $wp_inline_edit = inlineEditPost.edit;
-    //
-    // inlineEditPost.edit = function( id ) {
-    //
-    //     $wp_inline_edit.apply( this, arguments );
-    //
-    //     var $post_id = 0;
-    //     if ( typeof( id ) == 'object' ) {
-    //         $post_id = parseInt(this.getId(id));
-    //     }
-    //
-    //     if ( $post_id > 0 ) {
-    //         $('#support_inline_' + $post_id).children().each(function () {
-    //             var field = $('.form_field[name="' + $(this).attr('class') + '"]');
-    //
-    //             if(field.attr('type') == 'checkbox') {
-    //                 field.attr('checked', $(this).text() === 'on');
-    //             } else {
-    //                 field.val($(this).text());
-    //             }
-    //
-    //
-    //         });
-    //
-    //     }
-    //
-    // };
+    if(window.inlineEditPost != null) {
+
+        var $wp_inline_edit = inlineEditPost.edit;
+
+        inlineEditPost.edit = function (id) {
+
+            $wp_inline_edit.apply(this, arguments);
+
+            var $post_id = 0;
+            if (typeof( id ) == 'object') {
+                $post_id = parseInt(this.getId(id));
+            }
+
+            if ($post_id > 0) {
+                $('#support_inline_' + $post_id).children().each(function () {
+                    var field = $('.form_field[name="' + $(this).attr('class') + '"]');
+
+                    if (field.attr('type') == 'checkbox') {
+                        field.attr('checked', $(this).text() === 'on');
+                    } else {
+                        field.val($(this).text());
+                    }
+
+
+                });
+
+            }
+
+        };
+
+    }
 
     $.wpMediaUploader({
         target : '#support_login_logo',
