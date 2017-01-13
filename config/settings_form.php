e2676d015de8e576727a608ff442466f463e7162
Eric Green <eric@smartcat.ca>
2017-01-12 23:16:51 -0500

    Add settings form for frontend users

--- config/settings_form.php
+++ config/settings_form.php
@@ -8,10 +8,32 @@ $form = new Form( 'support_settings' );
 
 $form->add_field( new TextBoxField(
     array(
-        'id'                => 'password',
-        'type'              => 'password',
-        'label'             => __( 'New Password', PLUGIN_ID )
+        'id'        => 'firstname',
+        'label'     => __( 'First Name', PLUGIN_ID ),
+        'value'     => wp_get_current_user()->first_name
     )
+
+) )->add_field( new TextBoxField(
+    array(
+        'id'        => 'lastname',
+        'label'     => __( 'Last Name', PLUGIN_ID ),
+        'value'     => wp_get_current_user()->last_name
+    )
+
+) )->add_field( new TextBoxField(
+    array(
+        'id'        => 'new_password',
+        'type'      => 'password',
+        'label'     => __( 'New Password', PLUGIN_ID )
+    )
+
+) )->add_field( new TextBoxField(
+    array(
+        'id'        => 'confirm_password',
+        'type'      => 'password',
+        'label'     => __( 'Confirm Password', PLUGIN_ID )
+    )
+
 ) );
 
 return $form;
--- templates/navbar.php
+++ templates/navbar.php
@@ -31,7 +31,7 @@ $user = wp_get_current_user();
                             <ul class="dropdown-menu">
 
                                 <li>
-                                    <a href="#" class="alignright background-secondary hover menu-item">
+                                    <a href="<?php echo admin_url( 'admin-ajax.php' ); ?>?action=support_settings" rel="modal:open" class="alignright background-secondary hover menu-item">
                                         <span class="glyphicon glyphicon-cog"></span>
                                         <?php _e( 'Settings', SmartcatSupport\PLUGIN_ID ); ?>
                                     </a>    
