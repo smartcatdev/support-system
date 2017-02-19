7dc36a39a14333a58e1f5764b72ff4db2c43cd76
Eric Green <eric@smartcat.ca>
2017-02-19 13:41:24 -0500

    remove unuded class

--- includes/util/TicketUtils.php
+++ includes/util/TicketUtils.php
@@ -6,12 +6,6 @@ namespace SmartcatSupport\util;
 class TicketUtils {
     private function __construct() {}
 
-    public static function comments_enabled( $ticket ) {
-        $status = get_post_meta( $ticket, 'status', true );
-
-        return $status !== 'closed' && $status !== 'resolved';
-    }
-
     public static function ticket_ids() {
         return wp_list_pluck( get_posts( array( 'post_type' => 'support_ticket' ) ), 'ID' );
     }
