e71bc122e7e26ed119f485c725409724eb4777f7
Eric Green <eric@smartcat.ca>
2017-02-22 13:32:08 -0500

    Css fix

--- templates/ticket_list.php
+++ templates/ticket_list.php
@@ -2,13 +2,15 @@
 
 
 
-<div class="list-group">
+<div class="list-group ticket-list">
 
     <?php foreach( $query->posts as $post ) : ?>
 
-        <div class="list-group-item">
+        <div class="list-group-item ticket">
 
-            <a class="open-ticket" href="#" data-id="<?php echo $post->ID; ?>"># <?php echo $post->ID; ?> <?php echo $post->post_title; ?></a>
+            <a class="open-ticket" href="#" data-id="<?php echo $post->ID; ?>"><h4 class="ticket-title"><?php echo $post->post_title; ?></h4></a>
+
+            <p class="muted"># <?php echo $post->ID; ?> opened by <?php echo get_the_author_meta( 'display_name', $post->post_author ); ?></p>
 
         </div>
 
