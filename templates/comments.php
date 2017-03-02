a6912683a379579a9a632779c60d6ae1133fac0b
Eric Green <Eric Green>
2017-03-02 12:58:52 -0500

    Fix comments.php file path

--- templates/comments.php
+++ templates/comments.php
@@ -6,6 +6,6 @@ use SmartcatSupport\Plugin;
 
 <?php foreach( $comments as $comment ) : ?>
 
-    <?php include Plugin::plugin_dir( \SmartcatSupport\PLUGIN_ID ) . '\templates\comment.php'; ?>
+    <?php include Plugin::plugin_dir( \SmartcatSupport\PLUGIN_ID ) . '/templates/comment.php'; ?>
 
 <?php endforeach; ?>
