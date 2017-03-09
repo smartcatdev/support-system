02dd4d3c0d0b59fc267610181521c3b5c4d0854d
Eric Green <Eric Green>
2017-03-09 16:33:44 -0500

    Revert php max int for invalid author

--- includes/ajax/Ticket.php
+++ includes/ajax/Ticket.php
@@ -280,9 +280,9 @@ class Ticket extends AjaxComponent {
         $args['s'] = isset( $_REQUEST['search'] ) ? $_REQUEST['search'] : '';
 
         if( $form->is_valid() ) {
-error_log(serialize($form->data));
+
             $author = get_user_by( 'email', $form->data['email'] );
-            $args['author'] = $author ? $author->ID : PHP_INT_MAX;
+            $args['author'] = $author ? $author->ID : 0;
 
             unset( $form->data['email'] );
 
