1099fc7726ecd74a626968a24ecf4a5656e5bc1b
Eric Green <Eric Green>
2017-04-06 12:56:35 -0400

    Send replies from front and backend

--- includes/component/Notifications.php
+++ includes/component/Notifications.php
@@ -18,21 +18,21 @@ class Notifications extends AbstractComponent {
         Mailer::send_template( get_option( Option::WELCOME_EMAIL_TEMPLATE ), $user_data['email'], $user_data );
     }
 
-    public function ticket_updated( $ticket ) {
-        $recipient = get_user_by('id', $ticket->post_author );
+    public function ticket_updated( $null, $ticket_id, $key, $value ) {
+        if( $key == 'status' && $value == 'closed' ) {
+            $ticket = get_post( $ticket_id );
+            $recipient = get_user_by('id', $ticket->post_author );
 
-        $template_vars = array(
-            'ticket_subject' => $ticket->post_title,
-            'ticket_number'  => $ticket->ID,
-        );
+            $template_vars = array(
+                'ticket_subject' => $ticket->post_title,
+                'ticket_number' => $ticket->ID,
+                'ticket_status' => $value
+            );
 
-        $status =  get_post_meta( $ticket->ID, 'status', true );
-
-        if( $status == 'closed' || $status === 'resolved' ) {
-            $template_vars['ticket_status'] = $status;
+            Mailer::send_template(get_option(Option::UPDATED_EMAIL_TEMPLATE), $recipient->user_email, $template_vars);
         }
 
-        Mailer::send_template( get_option( Option::UPDATED_EMAIL_TEMPLATE ), $recipient->user_email, $template_vars );
+        return $null;
     }
 
     public function ticket_created( $ticket ) {
@@ -46,8 +46,10 @@ class Notifications extends AbstractComponent {
         Mailer::send_template( get_option( Option::CREATED_EMAIL_TEMPLATE ), $recipient, $template_vars );
     }
 
-    public function ticket_reply( $comment, $ticket ) {
+    public function ticket_reply( $comment_id ) {
         if( current_user_can( 'manage_support_tickets' ) ) {
+            $comment = get_comment( $comment_id );
+            $ticket = get_post( $comment->comment_post_ID );
             $recipient = get_user_by('id', $ticket->post_author );
 
             $template_vars = array(
@@ -76,8 +78,8 @@ class Notifications extends AbstractComponent {
         return array(
             'support_user_registered' => array( 'user_register' ),
             'support_ticket_created' => array( 'ticket_created' ),
-            'support_ticket_reply' => array( 'ticket_reply', 10, 2 ),
-            'support_ticket_updated' => array( 'ticket_updated' ),
+            'comment_post' => array( 'ticket_reply' ),
+            'update_post_metadata' => array( 'ticket_updated', 10, 4 ),
 
             'comment_notification_recipients' => array( 'disable_wp_notifications', 10, 2 ),
             'comment_moderation_recipients' => array( 'disable_wp_notifications', 10, 2 )
