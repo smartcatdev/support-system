5c82837528c9369eddbb8b776b8bd650fc20da29
Eric Green <Eric Green>
2017-04-07 10:29:19 -0400

    Allow filtered html in widgets

--- lib/admin/HTMLFilter.php
+++ lib/admin/HTMLFilter.php
@@ -6,6 +6,6 @@ namespace smartcat\admin;
 class HTMLFilter implements ValidationFilter {
 
     public function filter( $value ) {
-        return wp_filter_kses( $value );
+        return wp_filter_post_kses( $value );
     }
 }
