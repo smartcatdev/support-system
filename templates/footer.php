e169e1dd8b4dc3978a253ffb51469cc4fb0adc9f
Eric Green <Eric Green>
2017-04-13 16:42:33 -0400

    Fix memory leak

--- templates/footer.php
+++ templates/footer.php
@@ -5,101 +5,101 @@ use SmartcatSupport\descriptor\Option;
 $ver = get_option( Option::PLUGIN_VERSION );
 
 ?>
+        <footer id="footer">
 
-<footer id="footer">
+            <div class="container">
 
-    <div class="container">
+                <hr>
 
-        <hr>
+                <p class="footer-text text-center">
 
-        <p class="footer-text text-center">
+                    <?php $footer_text = get_option( Option::FOOTER_TEXT, Option\Defaults::FOOTER_TEXT ); ?>
 
-            <?php $footer_text = get_option( Option::FOOTER_TEXT, Option\Defaults::FOOTER_TEXT ); ?>
+                    <?php echo !empty( $footer_text ) ? $footer_text . ' |' : ''; ?>
 
-            <?php echo !empty( $footer_text ) ? $footer_text . ' |' : ''; ?>
+                    <a href="http://ucaresupport.com" target="_blank">
 
-            <a href="http://ucaresupport.com" target="_blank">
+                        <?php _e( ' Powered by uCare Support', \SmartcatSupport\PLUGIN_ID ); ?>
 
-                <?php _e( ' Powered by uCare Support', \SmartcatSupport\PLUGIN_ID ); ?>
+                    </a>
 
-            </a>
+                </p>
 
-        </p>
+            </div>
 
-    </div>
+        </footer>
 
-</footer>
+        <script>
 
-<script>
+            var Globals = {
+                ajax_url: "<?php echo admin_url( 'admin-ajax.php' ); ?>",
+                ajax_nonce: "<?php echo wp_create_nonce( 'support_ajax' ); ?>",
+                refresh_interval: <?php echo get_option( Option::REFRESH_INTERVAL, Option\Defaults::REFRESH_INTERVAL ); ?>,
+                strings: {
+                    loading_tickets: "Loading Tickets...",
+                    loading_generic: "Loading..."
+                }
+            };
 
-    var Globals = {
-        ajax_url: "<?php echo admin_url( 'admin-ajax.php' ); ?>",
-        ajax_nonce: "<?php echo wp_create_nonce( 'support_ajax' ); ?>",
-        strings: {
-            loading_tickets: "Loading Tickets...",
-            loading_generic: "Loading..."
-        }
-    };
+        </script>
 
-</script>
+        <script type="text/template" class="ajax-loader-mask">
 
-<script type="text/template" class="ajax-loader-mask">
+            <div class="ajax-loader">
 
-    <div class="ajax-loader">
+                <div class="dot-container">
 
-        <div class="dot-container">
+                    <div class="dot dot-1"></div>
 
-            <div class="dot dot-1"></div>
+                </div>
 
-        </div>
+                <div class="dot-container">
 
-        <div class="dot-container">
+                    <div class="dot dot-2"></div>
 
-            <div class="dot dot-2"></div>
+                </div>
 
-        </div>
+                <div class="dot-container">
 
-        <div class="dot-container">
+                    <div class="dot dot-3"></div>
 
-            <div class="dot dot-3"></div>
+                </div>
 
-        </div>
+                <p class="text-center"><%= obj %></p>
 
-        <p class="text-center"><%= obj %></p>
+            </div>
 
-    </div>
+        </script>
 
-</script>
+        <script type="text/template" class="notice-inline">
 
-<script type="text/template" class="notice-inline">
+            <div style="border-radius: 0; margin: 0" class="alert alert-success fade in">
 
-    <div style="border-radius: 0; margin: 0" class="alert alert-success fade in">
+                <a href="#" class="close" data-dismiss="alert">×</a><%= obj %>
 
-        <a href="#" class="close" data-dismiss="alert">×</a><%= obj %>
+            </div>
 
-    </div>
+        </script>
 
-</script>
+            <script src="<?php echo home_url( 'wp-includes/js/underscore.min.js' ) . '?ver=' . $ver; ?>"></script>
+            <script src="<?php echo $url . '/assets/lib/bootstrap/js/bootstrap.min.js' . '?ver=' . $ver; ?>"></script>
+            <script src="<?php echo $url . 'assets/lib/scrollingTabs/scrollingTabs.min.js' . '?ver=' . $ver; ?>"></script>
+            <script src="<?php echo $url . 'assets/lib/dropzone/js/dropzone.min.js' . '?ver=' . $ver; ?>"></script>
+            <script src="<?php echo $url . 'assets/lib/lightGallery/js/lightgallery.min.js' . '?ver=' . $ver; ?>"></script>
+            <script src="<?php echo $url . 'assets/lib/lightGallery/plugins/lg-zoom.min.js' . '?ver=' . $ver; ?>"></script>
+            <script src="<?php echo $url . 'assets/lib/moment/moment.min.js' . '?ver=' . $ver ?>" ></script>
+            <script src="<?php echo $url . 'assets/js/plugins.js' . '?ver=' . $ver ?>" ></script>
+            <script src="<?php echo $url . 'assets/js/app.js' . '?ver=' . $ver ?>" ></script>
+            <script src="<?php echo $url . 'assets/js/settings.js' . '?ver=' . $ver ?>" ></script>
+            <script src="<?php echo $url . 'assets/js/ticket.js' . '?ver=' . $ver ?>" ></script>
+            <script src="<?php echo $url . 'assets/js/comment.js' . '?ver=' . $ver ?>" ></script>
 
-<script src="<?php echo home_url( 'wp-includes/js/underscore.min.js' ) . '?ver=' . $ver; ?>"></script>
-<script src="<?php echo $url . '/assets/lib/bootstrap/js/bootstrap.min.js' . '?ver=' . $ver; ?>"></script>
-<script src="<?php echo $url . 'assets/lib/scrollingTabs/scrollingTabs.min.js' . '?ver=' . $ver; ?>"></script>
-<script src="<?php echo $url . 'assets/lib/dropzone/js/dropzone.min.js' . '?ver=' . $ver; ?>"></script>
-<script src="<?php echo $url . 'assets/lib/lightGallery/js/lightgallery.min.js' . '?ver=' . $ver; ?>"></script>
-<script src="<?php echo $url . 'assets/lib/lightGallery/plugins/lg-zoom.min.js' . '?ver=' . $ver; ?>"></script>
-<script src="<?php echo $url . 'assets/lib/moment/moment.min.js' . '?ver=' . $ver ?>" ></script>
-<script src="<?php echo $url . 'assets/js/plugins.js' . '?ver=' . $ver ?>" ></script>
-<script src="<?php echo $url . 'assets/js/app.js' . '?ver=' . $ver ?>" ></script>
-<script src="<?php echo $url . 'assets/js/settings.js' . '?ver=' . $ver ?>" ></script>
-<script src="<?php echo $url . 'assets/js/ticket.js' . '?ver=' . $ver ?>" ></script>
-<script src="<?php echo $url . 'assets/js/comment.js' . '?ver=' . $ver ?>" ></script>
+        <script>
 
-<script>
+            Dropzone.prototype.defaultOptions.maxFilesize = <?php echo get_option( Option::MAX_ATTACHMENT_SIZE, Option\Defaults::MAX_ATTACHMENT_SIZE ); ?>;
 
-    Dropzone.prototype.defaultOptions.maxFilesize = <?php echo get_option( Option::MAX_ATTACHMENT_SIZE, Option\Defaults::MAX_ATTACHMENT_SIZE ); ?>;
+        </script>
 
-</script>
-
-</body>
+    </body>
 
 </html>
