93fcd40a0017d191a59c9939ff098996b6547ce5
Eric Green <Eric Green>
2017-04-20 14:03:57 -0400

    Create default pasword reset template

--- migrations/migration-1_1_1.php
+++ migrations/migration-1_1_1.php
@@ -1,5 +1,7 @@
 <?php
 
+use SmartcatSupport\descriptor\Option;
+
 class migration_1_1_1 implements \SmartcatSupport\util\Migration {
 
     function version () {
@@ -7,10 +9,30 @@ class migration_1_1_1 implements \SmartcatSupport\util\Migration {
     }
 
     /**
+     * 1. Insert the default password reset template
      *
      * @return bool|WP_Error
      */
     function migrate () {
+        try {
+            $dir = \SmartcatSupport\plugin_dir();
+
+            $id = wp_insert_post(
+                array(
+                    'post_type'     => 'email_template',
+                    'post_status'   => 'publish',
+                    'post_title'    => __( 'Your password has been changed', \SmartcatSupport\PLUGIN_ID ),
+                    'post_content'  => file_get_contents( $dir . 'emails/password-reset.html' )
+                )
+            );
+
+            if( !empty( $id ) ) {
+                update_post_meta( $id, 'styles', file_get_contents( $dir . '/emails/default-style.css' ) );
+                update_option( Option::PASSWORD_RESET_EMAIL, $id );
+            }
+
+        } catch(Exception $ex) { }
+
         return true;
     }
 
