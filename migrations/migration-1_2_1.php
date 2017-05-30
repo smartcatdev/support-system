2b7605f67cdd5b6b62f5bcacf3aaca6c89ef46c9
Eric Green <Eric Green>
2017-05-30 09:26:40 -0400

    Add migration to create custom table

---
+++ migrations/migration-1_2_1.php
@@ -0,0 +1,58 @@
+<?php
+
+use ucare\descriptor\Option;
+
+class migration_1_2_1 implements \smartcat\core\Migration {
+
+    private $plugin;
+
+    function version() {
+        return '1.2.1';
+    }
+
+    function migrate( $plugin )  {
+
+        $this->plugin = $plugin;
+
+        $this->create_email_template();
+        $this->create_log_table();
+
+        return array( 'success' => true, 'message' => 'uCare has been successfully upgraded to version 1.2.0' );
+    }
+
+    function create_log_table() {
+
+        global $wpdb;
+
+        $wpdb->query(
+            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ucare_logs (
+                id          INT PRIMARY KEY AUTO_INCREMENT,
+                level       CHAR(1),
+                type        VARCHAR(30),
+                timestamp   DATETIME DEFAULT CURRENT_TIMESTAMP,
+                message     TEXT
+            )"
+        );
+    }
+
+    function create_email_template() {
+
+        $id = wp_insert_post(
+            array(
+                'post_type'     => 'email_template',
+                'post_status'   => 'publish',
+                'post_title'    => __( 'You have a ticket awaiting action', \ucare\PLUGIN_ID ),
+                'post_content'  => file_get_contents( $this->plugin->dir() . 'emails/ticket-close-warning.html' )
+            )
+        );
+
+        if( $id ) {
+            update_post_meta( $id, 'styles', file_get_contents( $this->plugin->dir() . 'emails/default-style.css' ) );
+            add_option( Option::INACTIVE_EMAIL, $id );
+        }
+
+    }
+
+}
+
+return new migration_1_2_1();
