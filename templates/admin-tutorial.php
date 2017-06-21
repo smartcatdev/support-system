13d76d2e7e5ed684515888b658bda0317c4241da
bilalhassan <info@ottawapc.cagit config --global user.email bilal@smartcat.cagit config --global user.name Bilal>
2017-06-21 16:44:25 -0400

    delete first run option on plugin de-activate

--- templates/admin-tutorial.php
+++ templates/admin-tutorial.php
@@ -1,3 +1,18 @@
-<?php
-
-echo 'test';
+<div id="ucare-tutorial">
+    <div class="wrap">
+        
+        <h1><?php echo __( 'Welcome to uCare Support System ', 'ucare' ) . ucare\PLUGIN_VERSION ?></h1>
+        
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</div>
