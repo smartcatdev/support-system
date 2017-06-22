302e78514cc746b4acadc6f183a62353dfea8a4e
bilalhassan <info@ottawapc.cagit config --global user.email bilal@smartcat.cagit config --global user.name Bilal>
2017-06-22 14:56:15 -0400

    tweaks to admin appearance

--- templates/admin-sidebar.php
+++ templates/admin-sidebar.php
@@ -4,7 +4,7 @@
         <h3><span class="dashicons dashicons-book"></span> <?php _e( 'Plugin Tutorial', 'ucare' ); ?></h3>
         <p><?php _e( 'Click the link below to view the plugin tutorial, as well as links to various helpful resources.', 'ucare' ); ?></p>
         <p>
-            <a href="<?php echo esc_url( admin_url( 'admin.php?page=uc-tutorial' ) ); ?>" class="button"><?php _e( 'Watch tutorial', 'ucare' ); ?></a>
+            <a href="<?php echo esc_url( admin_url( 'admin.php?page=uc-tutorial' ) ); ?>" class="ucare-button small secondary"><?php _e( 'Watch tutorial', 'ucare' ); ?></a>
         </p>
     </div>
     
@@ -13,7 +13,7 @@
         <p><?php _e( 'Help us make this plugin better! Please provide us comments and suggestions based on your experience or any bugs you come across. Feel free to'
                 . ' tell us about any features that will improve this plugin.', 'ucare' ); ?></p>
         <p>
-            <a href="https://wordpress.org/support/plugin/ucare-support-system/" target="_BLANK" class="button"><?php _e( 'Provide feedback', 'ucare' ); ?></a>
+            <a href="https://wordpress.org/support/plugin/ucare-support-system/" target="_BLANK" class="ucare-button small secondary"><?php _e( 'Provide feedback', 'ucare' ); ?></a>
         </p>
     </div>
     
@@ -21,7 +21,7 @@
         <h3><span class="dashicons dashicons-star-filled"></span> <?php _e( 'Support this plugin', 'ucare' ); ?></h3>
         <p><?php _e( 'Do you like this plugin? Support it by leaving a 5-star review on WordPress.org', 'ucare' ); ?></p>
         <p>
-            <a href="https://wordpress.org/support/plugin/ucare-support-system/reviews/" target="_BLANK" class="button"><?php _e( 'Leave review', 'ucare' ); ?></a>
+            <a href="https://wordpress.org/support/plugin/ucare-support-system/reviews/" target="_BLANK" class="ucare-button small secondary"><?php _e( 'Leave review', 'ucare' ); ?></a>
         </p>
     </div>
     
