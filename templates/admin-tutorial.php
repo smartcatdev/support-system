a7fac3d61c2b71e6e953e9cc45275008262390f0
bilalhassan <info@ottawapc.cagit config --global user.email bilal@smartcat.cagit config --global user.name Bilal>
2017-06-26 20:37:39 -0400

    updated frontend widget css - add tutorial video playlist

--- templates/admin-tutorial.php
+++ templates/admin-tutorial.php
@@ -4,6 +4,9 @@
         <h1><?php echo __( 'Welcome to uCare Support System ', 'ucare' ) . ucare\PLUGIN_VERSION ?></h1>
         
         <p><?php _e( 'uCare is the most intelligent and user friendly WordPress support plugin.', 'ucare' ); ?></p>
+    
+        
+        <iframe width="100%" height="500" src="https://www.youtube.com/embed/videoseries?list=PLw53ZZqpo1H40lcqgS5guHSEGRrie71xn&amp;controls=1&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
         
     </div>
     
