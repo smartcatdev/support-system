8e07ff20294512d4c03719c80d01270d52998c84
Eric Green <ericgreen.developer@gmail.com>
2017-11-11 20:18:21 -0500

    Move some helper functions

--- includes/sanitize.php
+++ includes/sanitize.php
@@ -13,3 +13,16 @@ function sanitize_post_id( $id ) {
     return get_post( $id ) ? $id : '';
 
 }
+
+
+function sanitize_agent_id( $id ) {
+
+    $user = get_user_by( 'id', absint( $id ) );
+
+    if ( $user && $user->has_cap( 'manage_support_tickets' ) ) {
+        return $id;
+    }
+
+    return false;
+
+}
