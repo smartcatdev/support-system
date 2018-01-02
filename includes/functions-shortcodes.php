64d2e47d1764bd76026a0bb917be4ab0839208c4
Eric Green <ericgreen.developer@gmail.com>
2018-01-02 15:03:05 -0500

    checkpoint

--- includes/functions-shortcodes.php
+++ includes/functions-shortcodes.php
@@ -1,13 +1,27 @@
 <?php
-
+/**
+ * Functions for handling shortcodes.
+ *
+ * @since 1.4.2
+ * @package ucare
+ */
 namespace ucare;
 
 
 // Add a shortcode for the login page
-add_shortcode( 'support-login', 'ucare\shortcode_login_form' );
+add_shortcode( 'support-login', 'ucare\support_login_form' );
 
 
-function shortcode_login_form( $args = array() ) {
+/**
+ * Output a login form for the support page.
+ *
+ * @param array $args
+ * @param bool  $echo
+ *
+ * @since 1.4.2
+ * @return string
+ */
+function support_login_form( $args = array(), $echo = true ) {
 
     $defaults = array(
         'form_id'              => 'loginform',
@@ -19,22 +33,26 @@ function shortcode_login_form( $args = array() ) {
         'pw_reset_link_text'   => __( 'Forgot Password', 'ucare' ),
         'register_link_text'   => __( 'Register', 'ucare' ),
 
-        'label_password' => __( 'Password', 'ucare' ),
-        'label_username' => __( 'Username or Email Address', 'ucare' ),
-        'label_remember' => __( 'Remember Me', 'ucare' ),
-        'label_log_in'   => __( 'Login', 'ucare' ),
+        'label_password'       => __( 'Password', 'ucare' ),
+        'label_username'       => __( 'Username or Email Address', 'ucare' ),
+        'label_remember'       => __( 'Remember Me', 'ucare' ),
+        'label_log_in'         => __( 'Login', 'ucare' ),
 
-        'id_username' => 'user_login',
-        'id_password' => 'user_pass',
-        'id_remember' => 'rememberme',
-        'id_submit'   => 'wp-submit',
+        'id_username'          => 'user_login',
+        'id_password'          => 'user_pass',
+        'id_remember'          => 'rememberme',
+        'id_submit'            => 'wp-submit',
 
-        'value_username' => '',
-        'value_remember' => false
+        'value_username'       => '',
+        'value_remember'       => false
     );
 
-    $args = shortcode_atts( $defaults, $args, 'support-login' );
+    $output = buffer_template( 'shortcode-login', shortcode_atts( $defaults, $args, 'support-login' ) );
 
-    echo buffer_template( 'shortcode-login', $args );
+    if ( $echo ) {
+        echo $output;
+    }
+
+    return $output;
 
 }
