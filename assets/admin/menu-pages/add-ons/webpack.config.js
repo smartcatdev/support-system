3077981d8582dbea5da805b19b9832e22adbd2d9
Eric Green <ericgreen.developer@gmail.com>
2018-02-09 09:59:02 -0500

    Add webpack module

---
+++ assets/admin/menu-pages/add-ons/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path'),
+
+      CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const SRC_DIR = path.resolve(__dirname, 'src'),
+      OUT_DIR = path.resolve(__dirname, 'build');
+
+module.exports = {
+    entry: path.resolve(SRC_DIR, 'index.js'),
+    output: {
+        path: OUT_DIR,
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['build'])
+    ],
+    module: {
+        loaders: [
+            {
+                test:  /\.(js|jsx)$/,
+                loaders: ['babel-loader']
+            }
+        ]
+    }
+};
---
+++ assets/admin/menu-pages/add-ons/webpack.prod.js
@@ -0,0 +1,12 @@
+const common = require('./webpack.config'),
+      merge  = require('webpack-merge'),
+      Minify = require('babel-minify-webpack-plugin');
+
+module.exports = merge(common, {
+    output: {
+        filename: 'bundle.production.min.js'
+    },
+    plugins: [
+        new Minify()
+    ]
+});
